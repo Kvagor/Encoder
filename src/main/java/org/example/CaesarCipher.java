0c8a370b8b60010c2be467511b3c6b6c50b6a23e
Egor Belonozhenko <egor.belonozhenko.04@mail.ru>
2023-12-24 21:08:03 +0300

    init commit1

--- src/main/java/org/example/CaesarCipher.java
+++ src/main/java/org/example/CaesarCipher.java
@@ -1,8 +1,24 @@
 package org.example;
 
 public class CaesarCipher {
-    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
+            ".,\":!? +-*/\\@#$%^&(){}[];'|`~=_©«»—" + "0123456789";
+
     public String encrypt(String message, int key) {
-        return "";
+        StringBuilder builder = new StringBuilder();
+        for (char aChar : message.toCharArray()) {
+            int index = ALPHABET.indexOf(aChar);
+            if (index >= 0) {
+                int newIndex = (index + key) % ALPHABET.length();
+                char newChar = ALPHABET.charAt(newIndex + (newIndex < 0 ? ALPHABET.length() : 0));
+                builder.append(newChar);
+            }
+        }
+        return builder.toString();
+    }
+
+    public String decrypt(String message, int key) {
+        return encrypt(message,-key);
     }
 }
--- src/main/java/org/example/Encrypted.java
+++ src/main/java/org/example/Encrypted.java
@@ -1,11 +1,31 @@
 package org.example;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Encrypted {
-    public void encrypted(){
+    public void encrypted() throws IOException{
         /*
         Запросить путь к файлу,запросить ключ шифрования, запросить путь куда записывать результат
         через ресурсы открыть чтение, открыть ресурс для чтения , зашифровать Цезаря, результат записать в файл
         для записи, создать класс Decrypted и все в обратном порядке
          */
+
+        Scanner scanner = new Scanner(System.in);
+        Path path1 = Path.of(scanner.nextLine());
+        Path path2 = Path.of(scanner.nextLine());
+        try(FileReader reader = new FileReader(path1.toFile());
+            FileWriter writer = new FileWriter(path2.toFile()))
+        {
+            char[] buffer = new char[65536];
+            while (reader.ready())
+            {
+                int real = reader.read(buffer);
+                writer.write(buffer, 0, real);
+            }
+        }
     }
 }

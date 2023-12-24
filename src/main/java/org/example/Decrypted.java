c4244bb1a3d97627a53bf95f97c6c5d6e58a32e9
Egor Belonozhenko <egor.belonozhenko.04@mail.ru>
2023-12-24 22:12:49 +0300

    init commit2

--- src/main/java/org/example/Decrypted.java
+++ src/main/java/org/example/Decrypted.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Decrypted {
+    public void decrypted() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите путь к файлу для чтения зашифрованных данных: ");
+        Path path1 = Path.of(scanner.nextLine());
+        System.out.print("Введите путь к файлу для записи расшифрованных данных: ");
+        Path path2 = Path.of(scanner.nextLine());
+        System.out.print("Введите ключ шифрования: ");
+        int key = scanner.nextInt();
+        try (FileReader reader = new FileReader(path1.toFile());
+             BufferedReader bufferedReader = new BufferedReader(reader);
+             FileWriter writer = new FileWriter(path2.toFile())) {
+            String line;
+            CaesarCipher cs = new CaesarCipher();
+            while ((line = bufferedReader.readLine()) != null) {
+                line = cs.decrypt(line, key);
+                writer.write(line);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Decrypted en = new Decrypted();
+        en.decrypted();
+    }
 }
--- src/main/java/org/example/Encrypted.java
+++ src/main/java/org/example/Encrypted.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Encrypted {
-    public void encrypted() throws IOException{
+    public void encrypted() throws IOException {
         /*
         Запросить путь к файлу,запросить ключ шифрования, запросить путь куда записывать результат
         через ресурсы открыть чтение, открыть ресурс для чтения , зашифровать Цезаря, результат записать в файл
@@ -15,17 +17,26 @@ public class Encrypted {
          */
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите путь к файлу для чтения данных: ");
         Path path1 = Path.of(scanner.nextLine());
+        System.out.print("Введите путь к файлу для записи зашифрованных данных: ");
         Path path2 = Path.of(scanner.nextLine());
-        try(FileReader reader = new FileReader(path1.toFile());
-            FileWriter writer = new FileWriter(path2.toFile()))
-        {
-            char[] buffer = new char[65536];
-            while (reader.ready())
-            {
-                int real = reader.read(buffer);
-                writer.write(buffer, 0, real);
+        System.out.print("Введите ключ шифрования: ");
+        int key = scanner.nextInt();
+        try (FileReader reader = new FileReader(path1.toFile());
+             BufferedReader bufferedReader = new BufferedReader(reader);
+             FileWriter writer = new FileWriter(path2.toFile())){
+            String line;
+            CaesarCipher cs = new CaesarCipher();
+            while ((line = bufferedReader.readLine()) != null) {
+                line = cs.encrypt(line,key);
+                writer.write(line);
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException{
+        Encrypted en = new Encrypted();
+        en.encrypted();
     }
 }
